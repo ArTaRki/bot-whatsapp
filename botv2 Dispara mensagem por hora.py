064e558592c5b5002e3d1dd842c02b999a36d0a9
ArTaRki <116949889+ArTaRki@users.noreply.github.com>
2024-05-16 01:52:19 -0300

    Update botv2 Dispara mensagem por hora.py

--- botv2 Dispara mensagem por hora.py
+++ botv2 Dispara mensagem por hora.py
@@ -1,14 +1,1 @@
 
-            mensagem += "\n".join([f"⏰ {horario} ✅" for horario in horarios])
-
-            enviar_mensagem(mensagem)
-            ultimo_envio = datetime.now()  # Atualiza o horário do último envio
-            # Aguarda até o próximo minuto
-            while datetime.now().minute == 57:
-                time.sleep(1)
-        else:
-            time.sleep(10)  # Verifica a cada 10 segundos
-
-# Inicia o envio de mensagens
-enviar_mensagens()
-
